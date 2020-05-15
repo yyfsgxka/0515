750bad9d25c0d70fde5052c0ca76373d53074a12
YangYF <YangYF@DESKTOP-R1NKRMF>
2020-05-15 14:32:45 +0800

    0515

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -11,6 +11,7 @@ using namespace std;
 Mat gammaTransform(Mat &src, float k)
 
 {
+	Mat dstmat;
 	//建立查询表数组Lut，size==256 
 	unsigned char LUT[256];
 	//遍历[0，255] 1.下标值归一化到[0, 1]之间 2.output=input^r 3. Lut[i] = Output * 255
@@ -21,6 +22,7 @@ Mat gammaTransform(Mat &src, float k)
 	}
 
 	Mat result = src.clone();
+
 	if (src.channels() == 1)
 
 	{
@@ -31,63 +33,13 @@ Mat gammaTransform(Mat &src, float k)
 			*iterator = LUT[(*iterator)];
 		}
 	}
+
 	
 
 	return result;
 
 }
-bool EqualizeHist(Mat &srcM, Mat &dstM)
 
-{
-	int cn = srcM.channels();
-	int w = srcM.cols;
-	int h = srcM.rows;
-	Mat hsv;
-    cvtColor(srcM, hsv, CV_BGR2HSV);
-	const int hist_sz = 256;
-	int hist[hist_sz];
-	//初始化
-	memset(hist, 0, sizeof(hist));
-	uchar *ptr = hsv.data;
-	for (int y = 0; y<h; y++)
-	{
-		for (int x = 0; x<w; x++)
-		{
-			int idx = y*w*cn + x*cn + 2;
-			hist[ptr[idx]]++;
-		}
-	}
-	float scale = 255.f / (w*h);
-	int sum = 0;
-	uchar lut[hist_sz + 1];
-	for (int i = 0; i < hist_sz; i++)
-
-	{
-		sum += hist[i];
-		int val = cvRound(sum*scale);
-		val = MIN(val, 255);
-		val = MAX(val, 0);
-		lut[i] = val;
-	}
-
-	lut[0] = 0;
-	for (int y = 0; y<h; y++)
-	{
-		for (int x = 0; x<w; x++)
-
-		{
-
-			int idx = y*w*cn + x*cn + 2;
-
-			ptr[idx] = lut[ptr[idx]];
-
-		}
-
-	}
-	cvtColor(hsv, dstM, CV_HSV2BGR);
-
-	return true;
-}
 
 int main()
 {
@@ -114,14 +66,23 @@ int main()
 	
 
 	//练习二
-	Mat dstM;
-	Mat srcM = imread("etest.jpg");
 	
-	imshow("scrM", srcM);
+	IplImage *img_in = cvLoadImage("etest.jpg");
+	IplImage *img_red = cvCreateImage(cvGetSize(img_in), IPL_DEPTH_8U, 1);	
+	IplImage *img_blue = cvCreateImage(cvGetSize(img_in), IPL_DEPTH_8U, 1);	
+	IplImage *img_green = cvCreateImage(cvGetSize(img_in), IPL_DEPTH_8U, 1);	
+	IplImage *img_out = cvCreateImage(cvGetSize(img_in), IPL_DEPTH_8U, 3);
+	cvNamedWindow("img_in", CV_WINDOW_AUTOSIZE);	
+	cvShowImage("img_in", img_in); 	
+    cvSplit(img_in, img_blue, img_green, img_red, 0); 	
+	cvEqualizeHist(img_blue, img_blue);
+	cvEqualizeHist(img_green, img_green);	
+	cvEqualizeHist(img_red, img_red); 	
+	cvMerge(img_blue, img_green, img_red, 0, img_out);	
+	cvNamedWindow("img_out", CV_WINDOW_AUTOSIZE);	
+	cvShowImage("img_out", img_out);
 
-	EqualizeHist(srcM, dstM);
-	imshow("dstM", dstM);
-
+	
 	
 	//练习三
 	Mat inputM = imread("gtest.jpg",0);
